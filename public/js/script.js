6596c9de3adfc3ba707c159dc7664454a4f2609e
kirthiSaiT <kirthisai251@gmail.com>
2024-10-11 22:02:43 +0530

    fourth commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,19 @@
 let socket = io();
+const markers = {}; 
 
 if (navigator.geolocation) {
     navigator.geolocation.watchPosition(
         (position) => {
             const { latitude, longitude } = position.coords;
+
+            
+            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+            
             socket.emit("send-location", { latitude, longitude });
         },
         (error) => {
-            console.error(error);
+            console.error("Geolocation error:", error);
         },
         {
             enableHighAccuracy: true,
@@ -15,8 +21,33 @@ if (navigator.geolocation) {
             maximumAge: 0,
         }
     );
+} else {
+    console.error("Geolocation is not supported by this browser.");
 }
 
 
 const map = L.map("map").setView([0, 0], 10);
 
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+
+    
+    console.log(`Received Location - ID: ${id}, Latitude: ${latitude}, Longitude: ${longitude}`);
+
+    
+    map.setView([latitude, longitude], 16);
+
+    if (markers[id]) {
+        
+        markers[id].setLatLng([latitude, longitude]);
+    } else {
+       
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
